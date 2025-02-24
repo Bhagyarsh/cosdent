4bbe6c2f4a3f6d18fecd07f8cd355bb7c6f8eda1
bhagyarsh dhumal <bhagyarsh@gmail.com>
2025-02-24 22:49:19 +0530

    more changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,8 @@ export default defineNuxtConfig({
   ],
   modules: ['@nuxt/icon', '@nuxt/ui'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/cosdent/', // Replace <REPO_NAME> with your GitHub repo name
+  }
 })
